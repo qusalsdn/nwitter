33c62f0e4deca8665ddeb9869ee95a6df7b81858
MinWooByun <yja05092@gmail.com>
2022-08-24 15:39:10 +0900

    Setup
    Next, Firebase 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import firebase from "../src/firebase";
+
+console.log(firebase);
 
 const Home: NextPage = () => {
   return <h1>nwitter</h1>;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC81RZ87fQTbIF_2y0jvtteO_6f0tH3WXY",
+  authDomain: "nwitter-c3c4b.firebaseapp.com",
+  projectId: "nwitter-c3c4b",
+  storageBucket: "nwitter-c3c4b.appspot.com",
+  messagingSenderId: "481607387182",
+  appId: "1:481607387182:web:784f59bd300b9c2702b7bf",
+};
+
+export default initializeApp(firebaseConfig);
