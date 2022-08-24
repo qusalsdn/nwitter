b2a73b940488e12ed920664be42549084ecbea95
MinWooByun <yja05092@gmail.com>
2022-08-24 16:01:55 +0900

    키 숨기기
    깃헙에 올려질 때 키를 숨기기 위한 작업

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC81RZ87fQTbIF_2y0jvtteO_6f0tH3WXY",
-  authDomain: "nwitter-c3c4b.firebaseapp.com",
-  projectId: "nwitter-c3c4b",
-  storageBucket: "nwitter-c3c4b.appspot.com",
-  messagingSenderId: "481607387182",
-  appId: "1:481607387182:web:784f59bd300b9c2702b7bf",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGIN_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 export default initializeApp(firebaseConfig);
